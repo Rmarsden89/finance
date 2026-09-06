c23a7b3b451ee868be602557c857c7febe6ddbe5
Rmarsden89 <86067373+Rmarsden89@users.noreply.github.com>
2026-09-06 15:49:02 -0400

    Add models package

---
+++ src/finance/models/__init__.py
@@ -0,0 +1,13 @@
+"""Versioned model definitions."""
+
+from .core_business_v1 import (
+    CORE_BUSINESS_V1,
+    CompositeModelDefinition,
+    add_core_business_v1_scores,
+)
+
+__all__ = [
+    "CORE_BUSINESS_V1",
+    "CompositeModelDefinition",
+    "add_core_business_v1_scores",
+]
