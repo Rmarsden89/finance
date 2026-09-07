e77febd741fa03155be0002cb8ae0887e9b52245
Rmarsden89 <86067373+Rmarsden89@users.noreply.github.com>
2026-09-06 23:05:54 -0400

    Export long-growth V1 model

--- src/finance/models/__init__.py
+++ src/finance/models/__init__.py
@@ -10,4 +10,13 @@ __all__ = [
     "CORE_BUSINESS_V1",
     "CompositeModelDefinition",
     "add_core_business_v1_scores",
+    "LONG_GROWTH_V1",
+    "LongGrowthModelDefinition",
+    "add_long_growth_v1_scores",
 ]
+
+from .long_growth_v1 import (
+    LONG_GROWTH_V1,
+    LongGrowthModelDefinition,
+    add_long_growth_v1_scores,
+)
