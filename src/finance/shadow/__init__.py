b3360f4c454570293b44e661fd52c73012417230
Rmarsden89 <86067373+Rmarsden89@users.noreply.github.com>
2026-09-08 09:08:35 -0400

    Add shadow decision package

---
+++ src/finance/shadow/__init__.py
@@ -0,0 +1,15 @@
+from .decision import (
+    PortfolioPosition,
+    ShadowDecision,
+    ShadowDecisionPlan,
+    build_shadow_decision_plan,
+    load_portfolio_positions,
+)
+
+__all__ = [
+    "PortfolioPosition",
+    "ShadowDecision",
+    "ShadowDecisionPlan",
+    "build_shadow_decision_plan",
+    "load_portfolio_positions",
+]
